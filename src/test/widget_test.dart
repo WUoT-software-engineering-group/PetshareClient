bfbea7e92f2b448e0d78c3fda08053ea06206ff2
sebastianSzarafin <74315304+sebastianSzarafin@users.noreply.github.com>
2023-03-24 20:48:44 +0100

    Update widget_test.dart

--- src/test/widget_test.dart
+++ src/test/widget_test.dart
@@ -1,1 +1,9 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets(
+      'Simple test just for github actions purpouses - should be removed in the future',
+      ((widgetTester) async {
+    expect(find.text('Text that won\'t be found'), findsNothing);
+  }));
+}
