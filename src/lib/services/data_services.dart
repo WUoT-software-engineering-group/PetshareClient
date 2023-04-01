9db7c5c9314b430137ea85158f695783bbe98921
DawidSzpejna <dawid.szpejna13@gmail.com>
2023-04-01 23:55:56 +0200

    services: add basic class for services
    The basic class for contection with backed

---
+++ src/lib/services/data_services.dart
@@ -0,0 +1,8 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:http/http.dart' as http;
+
+class DataServices {
+  final String _baseUrl = 'https://pet-share-web-api-dev.azurewebsites.net';
+}
