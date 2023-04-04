cc5ac7508c9ddc7ef13784631570e9ff06a438e4
DawidSzpejna <dawid.szpejna13@gmail.com>
2023-04-04 19:36:16 +0200

    widget: change structure of pet tile
    Replace application counter as follow icon button.

--- src/lib/views/announcementPage/pet_tile.dart
+++ src/lib/views/announcementPage/pet_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:pet_share/models/announcement.dart';
+import 'package:pet_share/views/announcementPage/clickable_icon_button.dart';
 import 'package:pet_share/views/announcementPage/labeled_icon.dart';
 import 'package:pet_share/utils/app_colors.dart';
 
@@ -24,26 +25,18 @@ class _PetTileState extends State<PetTile> {
         borderRadius: const BorderRadius.all(Radius.circular(15)),
         child: ClipRRect(
           borderRadius: BorderRadius.circular(15),
-          child: Container(
-            color: Colors.white,
-            height: 300,
+          child: SizedBox(
+            height: 315,
             child: Column(
               children: [
-                // picture of animal
-                Expanded(
-                    flex: 5,
-                    child: Container(
-                      decoration: const BoxDecoration(
-                          image: DecorationImage(
-                              fit: BoxFit.fill,
-                              image: AssetImage('assets/pupic.jpg'))),
-                    )),
-
-                // description of announcement
-                Expanded(
-                  flex: 2,
-                  child: DescriptionTile(widget.announcement),
+                Container(
+                  height: 225,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage('assets/pupic.jpg'))),
                 ),
+                DescriptionTile(announcement: widget.announcement),
               ],
             ),
           ),
@@ -55,7 +48,8 @@ class _PetTileState extends State<PetTile> {
 
 class DescriptionTile extends StatelessWidget {
   final Announcement announcement;
-  const DescriptionTile(this.announcement, {Key? key}) : super(key: key);
+  const DescriptionTile({required this.announcement, Key? key})
+      : super(key: key);
 
   String _ageOfPet(DateTime dateTime) {
     int years = DateTime.now().year - dateTime.year;
@@ -71,89 +65,87 @@ class DescriptionTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.fromLTRB(25, 12, 25, 5),
-      child: Row(
-        children: [
-          /* 1 */
-          Expanded(
-              flex: 5,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  /* 2 */
-                  RichText(
-                    text: TextSpan(
-                      style: DefaultTextStyle.of(context).style,
+    return Expanded(
+      child: Container(
+          padding: const EdgeInsets.fromLTRB(25, 8, 0, 8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        style: DefaultTextStyle.of(context).style,
+                        children: [
+                          TextSpan(
+                            text: announcement.pet.name,
+                            style: const TextStyle(
+                                fontSize: 20,
+                                color: Color.fromARGB(255, 70, 70, 70),
+                                fontWeight: FontWeight.w600),
+                          ),
+                          TextSpan(
+                            text: '    ${_ageOfPet(announcement.pet.birthday)}',
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.grey[700],
+                                fontWeight: FontWeight.w500),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 6,
+                    ),
+                    Row(
                       children: [
-                        TextSpan(
-                          text: announcement.pet.name,
-                          style: const TextStyle(
-                              fontSize: 20,
-                              color: Color.fromARGB(255, 70, 70, 70),
-                              fontWeight: FontWeight.w600),
+                        Icon(
+                          Icons.calendar_month,
+                          color: AppColors.smallElements['reddish'],
+                          size: 15,
                         ),
-                        TextSpan(
-                          text: '    ${_ageOfPet(announcement.pet.birthday)}',
+                        const SizedBox(
+                          width: 6,
+                        ),
+                        Text(
+                          DateFormat.yMMMd().format(announcement.creationDate),
                           style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.grey[700],
-                              fontWeight: FontWeight.w500),
-                        )
+                            color: Colors.grey[500],
+                          ),
+                        ),
                       ],
                     ),
-                  ),
-                  const SizedBox(
-                    height: 6,
-                  ),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.calendar_month,
-                        color: AppColors.smallElements['reddish'],
-                        size: 15,
-                      ),
-                      const SizedBox(
-                        width: 6,
-                      ),
-                      Text(
-                        DateFormat.yMMMd().format(announcement.creationDate),
-                        style: TextStyle(
-                          color: Colors.grey[500],
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 7,
-                  ),
-                  Row(
-                    children: [
-                      LabeledIcon(Icons.pets, announcement.pet.breed),
-                      const LabeledIcon(Icons.female, 'Female'),
-                    ],
-                  )
-                ],
-              )),
-          VerticalDivider(
-            thickness: 2,
-            indent: 10,
-            endIndent: 10,
-            color: Colors.grey[300],
-          ),
-          Expanded(
-            flex: 2,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.mail_outline,
-                    size: 30, color: AppColors.smallElements['reddish']),
-                const Text('0', style: TextStyle(fontSize: 16))
-              ],
-            ),
-          ),
-        ],
-      ),
+                    const SizedBox(
+                      height: 7,
+                    ),
+                    Row(
+                      children: [
+                        LabeledIcon(Icons.pets, announcement.pet.breed),
+                        const LabeledIcon(Icons.female, 'Female'),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              const VerticalDivider(
+                thickness: 2,
+                indent: 10,
+                endIndent: 10,
+                width: 0,
+              ),
+              SizedBox(
+                width: 120,
+                child: Center(
+                  child: ClickableIconButton(
+                      clicked: AppColors.buttons,
+                      nonClicked: AppColors.tile[1]),
+                ),
+              )
+            ],
+          )),
     );
   }
 }
