33da50b6057a9cd61dc72751f4609e703eff6a71
DawidSzpejna <dawid.szpejna13@gmail.com>
2023-04-18 18:50:07 +0200

    cubits: add features for auth0

--- src/lib/cubits/appCubit/app_cubit.dart
+++ src/lib/cubits/appCubit/app_cubit.dart
@@ -2,10 +2,12 @@ import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:pet_share/models/adopter.dart';
+import 'package:pet_share/services/auth_services.dart';
 import 'package:pet_share/services/data_services.dart';
-import 'package:pet_share/views/loginPage/users.dart';
 
 import '../../models/announcement.dart';
+import '../../views/authPage/users.dart';
 
 part 'app_state.dart';
 
@@ -13,24 +15,69 @@ class AppCubit extends Cubit<AppState> {
   AppCubit() : super(AppSInitial());
 
   final DataServices _dataServices = DataServices();
+  final AuthService _authService = AuthService();
 
-  void initShelter() async {
+  void authUser() async {
+    try {
+      bool signin = await _authService.authApp();
+
+      if (signin) {
+        switch (_authService.role) {
+          case UserRoles.adopter:
+            await initAdopter();
+            break;
+          case UserRoles.shelter:
+            await initShelter();
+            break;
+          case UserRoles.unassigned:
+            await initUnassigned();
+            break;
+          case UserRoles.unknown:
+            log('AppCubit:authUser: unknown role of user');
+            break;
+          default:
+            log('AppCubit: authUser: Somethings went wrong. Wrong role!');
+        }
+      }
+    } catch (e) {
+      log('AppCubit: authUser: ${e.toString()}');
+    }
+  }
+
+  String getEmail() {
+    return _authService.getEmail();
+  }
+
+  Future<void> setAddopter(Adopter adopter) async {
+    var idUser =
+        await _dataServices.addAdopter(adopter, _authService.accessToken!);
+    await _authService.setRole(UserRoles.adopter, idUser);
+    await initAdopter();
+  }
+
+  Future<void> initShelter() async {
     try {
       emit(AppSLoading());
       var res = await _dataServices.getAnnouncements();
       emit(AppSLoaded(res, UserType.shelter));
+      log('AppCubit: initShelter: init is done correctly.');
     } catch (e) {
       log(e.toString());
     }
   }
 
-  void initAdoptingPerson() async {
+  Future<void> initAdopter() async {
     try {
       emit(AppSLoading());
       var res = await _dataServices.getAnnouncements();
-      emit(AppSLoaded(res, UserType.adoptingPerson));
+      emit(AppSLoaded(res, UserType.adopter));
+      log('AppCubit: initAdopter: init is done correctly.');
     } catch (e) {
       log(e.toString());
     }
+  }
+
+  Future<void> initUnassigned() async {
+    emit(AppSAuthed());
   }
 }
--- src/lib/cubits/appCubit/app_state.dart
+++ src/lib/cubits/appCubit/app_state.dart
@@ -7,10 +7,20 @@ abstract class AppState extends Equatable {
   List<Object> get props => [];
 }
 
+/// The first state which is responsible
+/// for showing hello page.
 class AppSInitial extends AppState {}
 
+/// This state indicates the user has
+/// unassigned role.
+class AppSAuthed extends AppState {}
+
+/// This state is using to wait for
+/// loading all user's data after logging in.
 class AppSLoading extends AppState {}
 
+/// This state is related with user manger
+/// part of data flow in app.
 class AppSLoaded extends AppState {
   final List<Announcement> announcements;
   final UserType type;
